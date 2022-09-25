3f7fb2f0416c6337e1c04650510e920ee1320e8f
Jelle1212 <jellekomen7@gmail.com>
2022-09-25 12:39:08 +0200

    script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SBATCH --account=stud-ewi-crs-cese4010
+#SBATCH --partition=cese4010
+#SBATCH --qos=cese4010
+#SBATCH --reservation=cese4010
+#SBATCH --time=0:01:00
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=1024
+#SBATCH --mail-type=FAIL
+
+# Use this simple command to check that your sbatch settings are working
+/usr/bin/scontrol show job -d "$SLURM_JOB_ID"
+
+module use /opt/insy/modulefiles
+module load cuda/11.5
+
+srun run ./acsmatmult -h
